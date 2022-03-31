2d78f49b69c939d6f845b9e58da21faf8b970f8a
lutero.elavoco <lutero.elavoco@mutual.club>
2022-03-30 23:22:40 -0300

    feat: whatsapp bot remove unsuse provider

--- src/providers/implemententations/WhatsappBotProvider.ts
+++
@@ -1,9 +0,0 @@
-import { IWhatsappBotProvider } from '@providers/IWhatsappBotProvider';
-import { IVenom } from '@services/IVenom';
-
-export class WhatsappBotProvider implements IWhatsappBotProvider {
-  constructor(private venom: IVenom) {}
-  start() {
-    this.venom.initialize();
-  }
-}
